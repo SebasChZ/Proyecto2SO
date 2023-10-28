2515a3f19a3767e45a93a075cc3f84f337de92ff
Sebastian Chaves <sebas04@estudiantec.cr>
2023-10-28 09:00:55 -0600

    Delete

--- main.c
+++ main.c
@@ -28,6 +28,7 @@ void createArchive(const char *outputFile, const char *inputFiles[], int numFile
 void extractArchive(const char *archiveFile, const char *outputDirectory);
 void deleteFile(const char *archiveFile, const char *fileName);
 void listFiles(const char *archiveFile);
+void readData(const char *archiveFile);
 
 int main(int argc, char *argv[])
 {
@@ -60,11 +61,15 @@ int main(int argc, char *argv[])
     }
     else if (strcmp(opciones, "--delete") == 0)
     {
-        // deleteFile(archivoSalida, archivos[0]);
+        deleteFile(archivoSalida, archivos[0]);
     }
     else if (strcmp(opciones, "-t") == 0)
     {
         listFiles(archivoSalida);
+    }
+    else if (strcmp(opciones, "-r") == 0)
+    {
+        readData(archivoSalida);
     }
     else
     {
@@ -73,6 +78,31 @@ int main(int argc, char *argv[])
     }
 
     return 0;
+}
+
+// TEST
+/*
+    Funcion para escribir el contenido de un archivo en el tar.
+    tarFileName es el nombre del archivo tar.
+    fileName es el nombre del archivo del cual se grabara el contenido en el body del tar.
+*/
+void writeFileContentToTar(const char *tarFileName, const char *fileName)
+{                                                           //! Reemplazar por append
+    int file = openFile(fileName, 0);                       // Abre el archivo que se va a escribir en el tar
+    struct File fileInfo = findFile(tarFileName, fileName); // Encuentra el archivo en el header
+    char buffer[fileInfo.size];                             // Buffer para guardar el contenido del archivo a guardar
+    read(file, buffer, sizeof(buffer));                     // Lee el contenido del archivo y lo guarda en el buffer
+    int tarFile = openFile(tarFileName, 0);
+    lseek(tarFile, fileInfo.start, SEEK_SET); // Coloca el puntero del tar donde de escribir.
+
+    if (write(tarFile, buffer, sizeof(buffer)) == -1)
+    {
+        perror("writeFileContentToTar: Error al escribir en el archivo");
+        close(tarFile);
+        exit(1);
+    }
+    close(file);
+    close(tarFile);
 }
 
 void createArchive(const char *outputFile, const char *inputFiles[], int numFiles)
@@ -210,8 +240,10 @@ void listFiles(const char *archiveFile)
     // Itera a través de los archivos en el encabezado y muestra su información
     for (int i = 0; i < MAX_FILES; i++)
     {
+
         if (header.fileList[i].fileName[0] == '\0')
         {
+            printf("Index deleted: %d\n", i);
             continue; // Salta los archivos vacíos
         }
 
@@ -221,7 +253,108 @@ void listFiles(const char *archiveFile)
         printf("Start Position: %ld\n", (long)header.fileList[i].start);
         printf("End Position: %ld\n", (long)header.fileList[i].end);
         printf("Deleted: %s\n", header.fileList[i].deleted ? "Yes" : "No");
+        printf("Index: %d\n", i);
         printf("\n");
+    }
+
+    close(fd);
+}
+
+void deleteFile(const char *archiveFile, const char *fileName)
+{
+    // Abre el archivo empaquetado en modo lectura y escritura
+    int fd = open(archiveFile, O_RDWR);
+
+    if (fd == -1)
+    {
+        perror("Error al abrir el archivo empaquetado");
+        exit(1);
+    }
+
+    // Lee el encabezado del archivo empaquetado
+    read(fd, &header, sizeof(struct Header));
+
+    // Busca el archivo por nombre en el encabezado
+    int fileIndex = -1;
+    for (int i = 0; i < MAX_FILES; i++)
+    {
+        if (strcmp(header.fileList[i].fileName, fileName) == 0)
+        {
+            fileIndex = i;
+            break;
+        }
+    }
+
+    if (fileIndex == -1)
+    {
+        fprintf(stderr, "Archivo no encontrado: %s\n", fileName);
+        close(fd);
+        exit(1);
+    }
+
+    // Marca el archivo como eliminado en el encabezado
+    header.fileList[fileIndex].deleted = 1;
+
+    // Limpia el contenido del archivo en el buffer
+    off_t start = header.fileList[fileIndex].start;
+    off_t end = header.fileList[fileIndex].end;
+    size_t fileSize = end - start;
+
+    // Buffer de ceros del mismo tamaño que el archivo
+    char *zeroBuffer = (char *)malloc(fileSize);
+    if (zeroBuffer == NULL)
+    {
+        perror("Error al asignar memoria para zeroBuffer");
+        close(fd);
+        exit(1);
+    }
+
+    memset(zeroBuffer, 0, fileSize);
+
+    // Seek to the start position and write zeros to delete the data
+    lseek(fd, start, SEEK_SET);
+    write(fd, zeroBuffer, fileSize);
+
+    // Liberar la memoria del buffer
+    free(zeroBuffer);
+
+    // Clear the file entry in the header
+    memset(&header.fileList[fileIndex], 0, sizeof(struct FileHeader));
+
+    // Escribe el encabezado actualizado en el archivo empaquetado
+    lseek(fd, 0, SEEK_SET);
+    write(fd, &header, sizeof(struct Header));
+
+    close(fd);
+}
+
+void readData(const char *archiveFile)
+{
+    // Abre el archivo empaquetado en modo lectura
+    fprintf(stderr, "Archivo no encontrado: %s\n", archiveFile);
+    int fd = open(archiveFile, O_RDONLY);
+
+    if (fd == -1)
+    {
+        perror("Error al abrir el archivo empaquetado");
+        exit(1);
+    }
+
+    // Lee el encabezado del archivo empaquetado
+    read(fd, &header, sizeof(struct Header));
+
+    // Seek past the header to the data section
+    lseek(fd, sizeof(struct Header), SEEK_SET);
+
+    // Read and print the data
+    char buffer[1024];
+    ssize_t bytesRead;
+
+    while ((bytesRead = read(fd, buffer, sizeof(buffer))) > 0)
+    {
+        // Process the data here, e.g., print it or analyze it
+        // In this example, we'll just print it
+        write(STDOUT_FILENO, buffer, bytesRead);
     }
 
     close(fd);
--- texto2.txt
+++ texto2.txt
@@ -1,1 +1,1 @@
-asdasdasd
+asd
